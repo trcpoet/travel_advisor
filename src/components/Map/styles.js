f61cacb2ea213f9eff76c25feb4efd4a2d84f7d8
Tahsin Rahman Chowdhury <tchowdhury29@gmail.com>
2024-10-15 17:55:30 -0500

    first commit

---
+++ src/components/Map/styles.js
@@ -0,0 +1,17 @@
+import { makeStyles } from '@mui/icons-material';
+
+export default makeStyles(() => ({
+  paper: {
+    padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px',
+  },
+  mapContainer: {
+    height: '85vh', width: '100%',
+  },
+  markerContainer: {
+    position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 },
+  },
+  pointer: {
+    cursor: 'pointer',
+  },
+}));
+// AIzaSyBDSR5oeBPf1KvcwimOeS9l0SYSj81JMeo
